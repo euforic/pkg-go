f85c27bcc4a8a5b5c5e7c4de4511f5f53bb9f389
euforic <cs@euforic.io>
2024-07-26 21:49:02 -0700

    update github action

--- mapsi/mapsi.go
+++ mapsi/mapsi.go
@@ -5,54 +5,63 @@ import (
 	"strings"
 )
 
-// Value is an interface that can be used to store any value in a map.
-type Result interface{}
+var (
+	// ErrKeyDoesNotExist is returned when a key does not exist in a map.
+	ErrKeyDoesNotExist = errors.New("key does not exist")
+	// ErrAssertionFailed is returned when a type assertion fails.
+	ErrAssertionFailed = errors.New("type assertion failed")
+)
 
 // Get gets the value of a key in a map. If the key does not exist, it returns an error.
-func Get(m map[string]interface{}, key string) (Result, error) {
-	// Split the key into head and the rest
-	segments := strings.SplitN(key, ".", 2)
+func Get[T any](m map[string]any, key string) (T, error) { //nolint:ireturn
+	var zero T
+	segments := strings.SplitN(key, ".", 2) //nolint:mnd
 	head := segments[0]
 
 	// Get value associated with head
 	value, ok := m[head]
 	if !ok {
-		return nil, errors.New("key does not exist")
+		return zero, ErrKeyDoesNotExist
 	}
 
 	// If key has no dot, return value
 	if len(segments) == 1 {
-		return value, nil
+		if v, ok := value.(T); ok {
+			return v, nil
+		}
+
+		return zero, ErrAssertionFailed
 	}
 
 	// If value is a nested map, call Get recursively
-	if nestedMap, ok := value.(map[string]interface{}); ok {
-		return Get(nestedMap, segments[1])
+	if nestedMap, ok := value.(map[string]any); ok {
+		return Get[T](nestedMap, segments[1])
 	}
 
 	// If value is not a nested map and key has dot, return error
-	return nil, errors.New("key does not exist")
+	return zero, ErrKeyDoesNotExist
 }
 
 // Set sets the value of a key in a map. If the key does not exist, it is created.
-func Set(m map[string]interface{}, key string, value interface{}) error {
-	segments := strings.SplitN(key, ".", 2)
+func Set[T any](m map[string]any, key string, value T) error {
+	segments := strings.SplitN(key, ".", 2) //nolint:mnd
 	head := segments[0]
 
 	if len(segments) == 1 {
 		m[head] = value
+
 		return nil
 	}
 
-	if nestedMap, ok := m[head].(map[string]interface{}); ok {
+	if nestedMap, ok := m[head].(map[string]any); ok {
 		return Set(nestedMap, segments[1], value)
 	}
 
 	// if head does not exist, create it
-	m[head] = make(map[string]interface{})
-	if nestedMap, ok := m[head].(map[string]interface{}); ok {
+	m[head] = make(map[string]any)
+	if nestedMap, ok := m[head].(map[string]any); ok {
 		return Set(nestedMap, segments[1], value)
 	}
 
-	return errors.New("key does not exist")
+	return ErrKeyDoesNotExist
 }
--- ptype/convert.go
+++ ptype/convert.go
@@ -1,14 +1,17 @@
 package ptype
 
-func Deref[T any](in *T) T {
+// Deref returns the value that in points to, or a zero value if in is nil.
+func Deref[T any](in *T) T { //nolint:ireturn
 	if in == nil {
 		out := new(T)
+
 		return *out
 	}
 
 	return *in
 }
 
+// Ptr returns a pointer to in.
 func Ptr[T any](in T) *T {
 	return &in
 }
